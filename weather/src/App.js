066fba6d78f827be864c7f6f41ac40062d7b4983
syeddilawarraza <129573591+syeddilawarraza@users.noreply.github.com>
2023-12-30 15:35:08 +0500

    commit

---
+++ weather/src/App.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import Wether from './Components/Wether';
+
+function App() {
+  return (
+    <div className="App">
+      <Wether/>
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ weather/src/Components/Wether.js
@@ -0,0 +1,80 @@
+import './style.css'
+import { useState } from 'react';
+const api = {
+  key: "596a3084e56bc987bc611f278e2ba935",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function Wether() {
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          console.log(result)
+          setWeather(result);
+          setQuery('');
+        })
+    }
+  }
+  const dateBuilder = (d) => {
+    let months = [
+      'January', 'February', 'March', 'April', 'May',
+      'June', 'July', 'August',
+      'September', 'October', 'November', 'December'
+    ];
+    let days = [
+      'sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturday'
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`
+  }
+  return (
+    <div className={(typeof weather.main !=
+     "undefined")?((weather.main.temp >16) ?
+     'app warm':'app'):'app'}>
+
+      <main>
+        <div className='search-box'>
+          <input type='text'
+            className='search-bar'
+            placeholder='Search...'
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            onKeyPress={search} />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>
+                {/* {weather.name},{weather.sys.country} */}
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className='date'>
+                {dateBuilder(new Date())} </div>
+
+            </div>
+            <div className='weather-box'>
+              <div className='temp'>
+                {Math.round(weather.main.temp)}°C
+
+              </div>
+                <div className='weather'>
+                  {weather.weather[0].main}
+                </div>
+            </div>
+          </div>
+        ) : ('')
+        }
+
+      </main >
+    </div >
+  );
+}
+
+export default Wether;
